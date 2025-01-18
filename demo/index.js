31d07d862ee57a761a0d96a0420fab9a5d779ce1
Anton Plauche <antonplauche92@gmail.com>
2025-01-18 11:20:51 -0600

    basic gmc setup

---
+++ demo/index.js
@@ -0,0 +1,56 @@
+import gsap from "gsap";
+import { gmc } from "../src/gsap-motion-composer.js";
+
+gmc.each('.icon-row', (block) => {
+  const items = block.querySelectorAll('.item') // Items are scoped to instance
+  gmc.onScroll.enter(block)
+    .from(items, {autoAlpha: 0, y: 32, stagger: 0.4, duration: 1})
+})
+
+gmc.each('.icon-row-with-reset', (block) => {
+  const items = block.querySelectorAll('.item') // Items are scoped to instance
+  gmc.onScroll.enterAndReset(block)
+    .from(items, {autoAlpha: 0, y: 32, stagger: 0.4, duration: 1})
+})
+
+gmc.each('.counter-row', (block) => {
+  const counter = block.querySelector('.counter-item')
+  gmc.onScroll.enter(block).gmc_count(counter, 10, 200, 2)
+})
+
+gmc.each('.parallax-row', (block) => {
+  const items = block.querySelectorAll('.item')
+  gmc.onScroll.scrub(block)
+    .from(items[0], {y: -300}, "0")
+    .from(items[1], {y: 200}, "0")
+    .from(items[2], {y: 400}, "0")
+})
+
+gmc.each('.parallax-pinned-row', (block) => {
+  const items = block.querySelectorAll('.item')
+  gmc.onScroll.scrubAndPin(block)
+    .from(items[0], {y: -300}, "0")
+    .from(items[1], {y: 200}, "0")
+    .from(items[2], {y: 400}, "0")
+})
+
+// gmc.each('.icon-row', (block) => {
+//   const items = block.querySelectorAll('.icon-row__item')
+//   const tl = gmc.scroll.enter(block).from(items, {autoAlpha: 0, y:32, stagger: 1, duration: 3})
+// })
+
+
+// gmc.each('.icon-row', (block) => {
+//   const items = block.querySelectorAll('.icon-row__item')
+//   const tl = gmc.scroll.enter(block).from(items, {autoAlpha: 0, y:32, stagger: 1, duration: 3})
+// })
+
+// gmc.each('.counter-row', (block) => {
+//   gmc.scroll.enter(block).from(block, {
+//     onUpdate: (this) => {
+//       gmc.onUpdate.count(this, '.count', 8, 80, 2)
+//     }
+//   })
+// })
+
+// gmc.recipes.pinnedImageSequence()
---
+++ src/gsap-motion-composer.js
@@ -0,0 +1,182 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { pinnedImageSequence } from './recipes/pinned-image-sequence';
+import { scrollingBodyClasses } from './recipes/scrolling-body-classes';
+
+const GSAPMotionComposer = () => {
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const scrollDefaults = {
+    start: "50% bottom",
+    scrub: false,
+    pin: false
+  }
+
+  gsap.core.Timeline.prototype.gmc_count = function (
+    el, 
+    start, 
+    end, 
+    decimals, 
+    options = {}, 
+    position = undefined
+  ){
+    let counter = {value: start}
+    this.to(counter, {
+        value: end,
+        duration: 2,
+        ease: "none",
+        onUpdate: () => {
+          el.innerText = counter.value.toFixed(decimals); 
+        },
+        ...options
+      }, position)
+    return this
+  }
+
+  /**
+   * Utility for doing a querySelectorAll and forEach loop and then running functionality
+   * 
+   * @param {String} selector selector for querySelectorAll() to find and execute for each loop on
+   * @param {Function} cb Function to execute scoped to each section
+   */
+  const each = (selector, cb) => {
+    const items = document.querySelectorAll(selector)
+    items.forEach(item => {
+      cb(item)
+    })
+  }
+
+  /**
+   * Scrolltrigger generators for quickly creating scroll based animations
+   */
+  const onScroll = {
+    /**
+     * A basic scrolltrigger that will play your chained animations when the section enters the viewport
+     * 
+     * @param {HTMLElement} section The section to execute the trigger on
+     * @param {GSAPTimeline} animation Optionally pass a preconfigured timeline to trigger
+     * @param {Object} options object containing extra scroll trigger configurations
+     * @returns {GSAPTimeline}
+     */
+    enter: (
+      section,
+      animation = false,
+      options = {}
+    ) => {
+      // If a timeline is not passed, create one and return it for chaining
+      let tl = animation ? animation : gsap.timeline({})
+
+      ScrollTrigger.create({
+        trigger: section,
+        ...scrollDefaults,
+        ...options,
+        animation: tl
+      })
+      return tl
+    },
+    /**
+     * A scrolltrigger with offscreen resets - pass in a timeline or tween 
+     * 
+     * @param {HTMLElement} section The section to execute the trigger on
+     * @param {GSAPTimeline} animation Optionally pass a preconfigured timeline to trigger
+     * @param {Object} options object containing extra scroll trigger configurations
+     * @returns {GSAPTimeline}
+     */
+    enterAndReset: (
+      section,
+      animation = false,
+      options = {}
+    ) => {
+      let tl = animation ? animation : gsap.timeline({})
+      tl.pause(0)
+      const playScrollTrigger = ScrollTrigger.create({
+        trigger: section,
+        ...scrollDefaults,
+        ...options,
+        onEnter: () => tl.play(),
+
+      })
+      const resetScrollTrigger = ScrollTrigger.create({
+        trigger: section,
+        onLeaveBack: () => tl.pause(0),
+        start: "top bottom",
+      })
+      return tl
+    },
+    /**
+     * A scrolltrigger that scrubs with scroll 
+     * 
+     * @param {HTMLElement} section The section to execute the trigger on
+     * @param {GSAPTimeline} animation Optionally pass a preconfigured timeline to trigger
+     * @param {Object} options object containing extra scroll trigger configurations
+     * @returns {GSAPTimeline}
+     */
+    scrub: (
+      section,
+      animation = false,
+      options = {}
+    ) => {
+      let tl = animation ? animation : gsap.timeline({})
+
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+        pin: false,
+        ...options,
+        animation: tl
+      })
+
+      return tl
+    },
+    /**
+     * A scrolltrigger that scrubs with scroll and pins section 
+     * 
+     * @param {HTMLElement} section The section to execute the trigger on
+     * @param {GSAPTimeline} animation Optionally pass a preconfigured timeline to trigger
+     * @param {Object} options object containing extra scroll trigger configurations
+     * @returns {GSAPTimeline}
+     */
+    scrubAndPin: (
+      section,
+      animation = false,
+      length = 500,
+      options = {}
+    ) => {
+      let tl = animation ? animation : gsap.timeline({})
+
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        end: `+=${length}`,
+        scrub: true,
+        pin: true,
+        ...options,
+        animation: tl
+      })
+
+      return tl
+    }
+  }
+
+
+  /**
+   * Pre-built and ready to go recipes - plug and play 
+   */
+  const recipes = {
+    pinnedImageSequence,
+    scrollingBodyClasses
+  }
+  
+
+  return {
+    each,
+    onScroll,
+    recipes
+  }
+}
+
+export const gmc = GSAPMotionComposer();
+
